ee5cdafdf57a194be7df3516eb7f5657d3a4f1f7
Joseph Lamb <jlambpco@gmail.com>
2022-11-18 09:03:32 -0500

    loading home page, intial routes built

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,2 +1,14 @@
 const router = require('express').Router()
 const db = require('../../db/db.json')
+const fs = require('fs')
+const app = require('express')
+
+const notesRoute = ('./notes')
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+router.use('/notes', notesRoute)
+
+module.exports = router
---
+++ routes/api/notes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const fs = require('fs')
+
+app.get('/', (req, res) =>
+
+
+
+
+
